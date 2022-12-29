a17d86d03d860c521a11f1671c58d7e42490c656
Lee <leewhiz119@gmail.com>
2022-12-29 14:24:35 -0500

    conditionally rendered backOfCard image into deck.js based on whether a deck prop had been passed down from parent

--- src/components/cards/Deck.js
+++ src/components/cards/Deck.js
@@ -4,11 +4,14 @@ import { useState } from 'react'
 
 export default function Deck(props) {
   const deck = props.deck
-
+  if(deck) {
   return (
     <div className="card">
     
-    <img style={{ height: 70, width: 50}} src="/images/cards/yugioh_back_of_card.webp" />
+    <img style={{ height: 120, width: 85}} src="/images/cards/yugioh_back_of_card.webp" />
     </div>
+
   )
+}
+   return <div className="card"></div>
 }
