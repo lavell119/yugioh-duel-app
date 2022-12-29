d6409c5ef76ff145a2c1c600d9173b7a8ea88c94
Lee <leewhiz119@gmail.com>
2022-12-29 15:03:34 -0500

    added card component to all card zones

--- src/components/cards/MonsterCard.js
+++ src/components/cards/MonsterCard.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
+import Card from './Card.js'
+
 
 export default function MonsterCard(props) {
 
   let [card, setCard] = useState(props.card)
   console.log(card)
   return (
-    <div className="card monster-card" style={{ 
-      backgroundImage: card, backgroundSize: "contain"
-    }}>
+    <div className="card monster-card">
+      <Card />
     </div>
   )
 }
--- src/components/cards/SpellTrapCard.js
+++ src/components/cards/SpellTrapCard.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Card from './Card.js'
+
 
 export default function SpellTrapCardk() {
   return (
-    <div className="card spell-trap-card"></div>
+    <div className="card spell-trap-card">
+      <Card />
+    </div>
   )
 }
