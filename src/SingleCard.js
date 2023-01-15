9e6904e7843ecfa819ab0f3b281d1ccabb08b093
Lee <leewhiz119@gmail.com>
2023-01-14 20:44:36 -0500

    used useEffect to prevent infinite renders when setting that deck state in SingleCard

--- src/SingleCard.js
+++ src/SingleCard.js
@@ -3,24 +3,38 @@ import { useParams } from 'react-router-dom'
 import KaibaDeck from './components/kaiba_deck.json'
 import YugiDeck from './components/kaiba_deck.json'
 import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
 
 export default function SingleCard(props) {
-    const [cardDeck, setCardDeck] =  useState('')
-    if(props.deck === "kaiba"){
-        setCardDeck(KaibaDeck)
-        console.log(cardDeck)
-    } 
+    const [cardDeck, setCardDeck] =  useState()
+    // if(props.deck === "kaiba"){
+    //     setCardDeck(KaibaDeck)
+    //     console.log(cardDeck)
+    // } 
 
-    if(props.deck === "yugi"){
-        setCardDeck(YugiDeck)
-        console.log(cardDeck)
+    // if(props.deck === "yugi"){
+    //     setCardDeck(YugiDeck)
+    //     console.log(cardDeck)
+    // }
+    function setDeck (){
+    if(props.deck === "kaiba"){
+         setCardDeck(KaibaDeck)
+         console.log(cardDeck)
+    } else return
+
+    // if(props.deck === "yugi"){
+    //      setCardDeck(YugiDeck)
+    //      console.log(cardDeck)
+    // } else return
     }
 
+    useEffect(()=>setDeck())
   
     console.log(props)
     const { id } = useParams()
     console.log(id)
   return (
-    <div>SingleCard</div>
+    <div style={{color:"white", height: 300, width: 300, background: "red"}}>wekfnwelkng</div>
   )
 }
