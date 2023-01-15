aa77a55aefa21bffec7362761c8c50b5e9627784
Lee <leewhiz119@gmail.com>
2023-01-14 20:18:45 -0500

    add conditional checks inside SingleCard to check the deck and console log it

--- src/SingleCard.js
+++ src/SingleCard.js
@@ -2,10 +2,21 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import KaibaDeck from './components/kaiba_deck.json'
 import YugiDeck from './components/kaiba_deck.json'
+import { useState } from 'react'
 
 export default function SingleCard(props) {
-    console.log(KaibaDeck)
-    console.log(YugiDeck)
+    const [cardDeck, setCardDeck] =  useState('')
+    if(props.deck === "kaiba"){
+        setCardDeck(KaibaDeck)
+        console.log(cardDeck)
+    } 
+
+    if(props.deck === "yugi"){
+        setCardDeck(YugiDeck)
+        console.log(cardDeck)
+    }
+
+  
     console.log(props)
     const { id } = useParams()
     console.log(id)
