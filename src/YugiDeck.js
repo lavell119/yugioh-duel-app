c225bad9a7ec3b53014262b78c4abc42edd3db4e
Lee <leewhiz119@gmail.com>
2023-01-16 08:23:06 -0500

    added dynamic link to each card inside YugiDeck.js to single card page

--- src/YugiDeck.js
+++ src/YugiDeck.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Cards from './components/yugi_deck.json'
+import { Link } from 'react-router-dom'
 
 
 
@@ -10,9 +11,13 @@ export default function YugiDeck() {
   return (
     <>
     <h2>Kaiba Starter Deck</h2>
+    <Link to={"/kaibadeck"}>lllll</Link>
     <div className="cards-display">
         {Cards.map(card =>
           <div className="card-test">
+          <div class="card-title">{Cards.indexOf(card)}</div>
+          <Link to={`/yugideck/${Cards.indexOf(card)}`}>Link</Link>
+
             <div class="card-title">{card.name}</div>
             <img style={{ height: "150px", width: "100px" }}src={card.image} alt="" /> 
           </div>
