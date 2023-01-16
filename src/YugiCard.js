3d3b1b4888e8ea93bacae0dfe82dd7257c13d533
Lee <leewhiz119@gmail.com>
2023-01-16 11:57:43 -0500

    succesfully rendered attack points

--- src/YugiCard.js
+++ src/YugiCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import YugiDeck from './components/yugi_deck.json'
 import { useParams } from 'react-router-dom'
 
+let int = 2
 
 export default function YugiCard() {
     console.log(YugiDeck)
@@ -10,13 +11,17 @@ export default function YugiCard() {
     console.log(selectedCard)
 
 
+    let attackPoints = selectedCard.attack_points
+    console.log(attackPoints)
 
   return (
     <div className="single-card-snippet">
         <h3 class="white">{selectedCard.name}</h3>
         <img src={selectedCard.image} />
         <div className="card-info">
-            
+        {selectedCard.card_type==="monster" && 
+            <div class="white">Attack Points: {attackPoints}</div>
+            }     
         </div>
     </div>
   )
