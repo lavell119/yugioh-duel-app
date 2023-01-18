b0774a45626598629257c4db05e66c5d1b3df04b
Lee <leewhiz119@gmail.com>
2023-01-18 06:48:42 -0500

    started working on hand draw functionality in Hand.js

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import Card from './cards/Card.js'
+import KaibaDeck from './kaiba_deck.json'
 
 export default function Hand(props) {
-  
+  let numberOfCards = 50
+  let hand =[]
+  let drawHand = ()=>{
+
+    let e = Math.floor(Math.random() * numberOfCards)
+    let drawnCard=(KaibaDeck[e])
+    console.log(drawnCard)
+    numberOfCards=numberOfCards-1
+    hand.push(drawnCard)
+    KaibaDeck.splice(e, 1)
+    console.log(KaibaDeck)
+    console.log(hand)
+}
   console.log(props)
   let handCards = props.handCards
   console.log('handcards =' +{handCards})
+  
   return (
     <div className="hand">
+          <button onClick={drawHand}>Draw</button>
           {/* {handCards.map(card => <div className="card white"><img style={{height: 120}} src={card.image} /></div>)} */}
-          {handCards.map(card => <Card card={card}/>)}
+          {hand.map(card => <Card card={card}/>)}
 
       {/* <Card card="blue_eyes_white_dragon"/>
       <Card card="blue_eyes_white_dragon"/>
