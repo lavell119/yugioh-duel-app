d645a96c3315386155b3a68cb9f8a92f1a6d9c80
Lee <leewhiz119@gmail.com>
2023-01-18 17:47:12 -0500

    added handState state to Hand.js with useState and appended drawn card to handState inside drawHand function

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import Card from './cards/Card.js'
 import KaibaDeck from './kaiba_deck.json'
+import { useState } from 'react'
 
 export default function Hand(props) {
+  let [handState, setHandState] = useState([])
   let numberOfCards = 50
   let hand =[]
-  let drawHand = ()=>{
 
+  //Draw card function
+  let drawHand = ()=>{
     let e = Math.floor(Math.random() * numberOfCards)
     let drawnCard=(KaibaDeck[e])
     console.log(drawnCard)
     numberOfCards=numberOfCards-1
     hand.push(drawnCard)
+    setHandState([...handState, drawnCard])
+    console.log(handState)
     KaibaDeck.splice(e, 1)
     console.log(KaibaDeck)
     console.log(hand)
@@ -24,7 +29,7 @@ export default function Hand(props) {
     <div className="hand">
           <button onClick={drawHand}>Draw</button>
           {/* {handCards.map(card => <div className="card white"><img style={{height: 120}} src={card.image} /></div>)} */}
-          {hand.map(card => <Card card={card}/>)}
+          {handCards.map(card => <Card card={card}/>)}
 
       {/* <Card card="blue_eyes_white_dragon"/>
       <Card card="blue_eyes_white_dragon"/>
