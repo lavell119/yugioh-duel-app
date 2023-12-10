3ddd6bbc5e23d25e6161681def8b9c2a115719eb
Lee <leewhiz119@gmail.com>
2023-12-10 17:34:47 -0500

    added blue eyes and skull servant

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,9 @@ import KaibaDeck from '../KaibaDeck'
 import Field from './Field'
 import YugiDeck from  '../YugiDeck'
 import FiendDeck from '../FiendDeck'
+import LOBBooster from '../LOBBooster'
+
+
 
 export default function Navbar() {
   return (
@@ -14,7 +17,7 @@ export default function Navbar() {
         <Link to="/yugideck" element={<YugiDeck />}>Yugi Deck</Link>
         <Link to="/kaibadeck" element={<KaibaDeck />}>Kaiba Deck</Link>
         <Link to="/fienddeck" element={<FiendDeck />}>Fiend Deck</Link>
-
+        <Link to="/lobbooster" element={<LOBBooster />}>Legend of Blue Eyes White Dragon</Link>
         <Link to="/">Home</Link>
         <Link to="/field" element={<Field />}>Field</Link>
 
