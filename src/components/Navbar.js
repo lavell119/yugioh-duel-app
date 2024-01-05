453fb61432a3c4b21c01df6cb88758f7fe077bb3
Lee <leewhiz119@gmail.com>
2024-01-05 03:37:35 -0500

    Decks link to Navbar and route for Decks component in App.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import Field from './Field'
 import YugiDeck from  '../YugiDeck'
 import FiendDeck from '../FiendDeck'
 import LOBBooster from '../LOBBooster'
+import Decks from './Decks'
 
 
 
@@ -14,9 +15,7 @@ export default function Navbar() {
     <div className="navbar">
         <Link to="/">Home</Link>
         <Link to="/allcards" element={<AllCards />}>All Cards</Link>
-        <Link to="/yugideck" element={<YugiDeck />}>Yugi Deck</Link>
-        <Link to="/kaibadeck" element={<KaibaDeck />}>Kaiba Deck</Link>
-        <Link to="/fienddeck" element={<FiendDeck />}>Fiend Deck</Link>
+        <Link to="/decks" element={<Decks />}>Decks</Link>
         <Link to="/lobbooster" element={<LOBBooster />}>Legend of Blue Eyes White Dragon</Link>
         <Link to="/field" element={<Field />}>Field</Link>
 
